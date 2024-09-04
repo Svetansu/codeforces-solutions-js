1d6e231f544cd10dc3fc21f0691a77deca9073a0
Svetansu <30946134+Svetansu@users.noreply.github.com>
2024-09-04 21:30:22 +0530

    Jagged Swaps

---
+++ 1896-A.js
@@ -0,0 +1,33 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readline() {
+    return inputString[currentLine++];
+}
+
+// solution here
+
+function main() {
+    var t = parseInt(readline());
+
+    for (var a0 = 0; a0 < t; a0++) {
+        var n = parseInt(readline());
+        var p = readline().split(' ').map(x => parseInt(x));
+        console.log(p[0] == 1 ? 'YES' : 'NO');
+    }
+}
